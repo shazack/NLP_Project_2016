813006bc94485dd0f363b19087b0732994374c32
Nisha Shahul Hameed <nisharazack18@gmail.com>
2016-12-03 09:56:12 -0800

    Update and rename temp to bigram.py

---
+++ NaiveBayes/bigram.py
@@ -0,0 +1,9 @@
+
+def find_bigrams(input_list):
+  bigram_list = []
+  for i in range(len(input_list)-1):
+      bigram_list.append((input_list[i], input_list[i+1]))
+  return bigram_list
+
+
+print(find_bigrams(input_list))
