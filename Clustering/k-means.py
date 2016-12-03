d1204c891aa2db47e1eadec942e523c49464230e
Nisha Shahul Hameed <nisharazack18@gmail.com>
2016-12-03 09:59:07 -0800

    Update k-means.py

--- Clustering/k-means.py
+++ Clustering/k-means.py
@@ -25,3 +25,44 @@ for cluster in xrange(0,10):
         if( word_centroid_map.values()[i] == cluster ):
             words.append(word_centroid_map.keys()[i])
     print words
+
+def create_bag_of_centroids( wordlist, word_centroid_map ):
+    #
+    # The number of clusters is equal to the highest cluster index
+    # in the word / centroid map
+    num_centroids = max( word_centroid_map.values() ) + 1
+    #
+    # Pre-allocate the bag of centroids vector (for speed)
+    bag_of_centroids = np.zeros( num_centroids, dtype="float32" )
+    #
+    # Loop over the words in the review. If the word is in the vocabulary,
+    # find which cluster it belongs to, and increment that cluster count 
+    # by one
+    for word in wordlist:
+        if word in word_centroid_map:
+            index = word_centroid_map[word]
+            bag_of_centroids[index] += 1
+    #
+    # Return the "bag of centroids"
+    return bag_of_centroids
+
+# Pre-allocate an array for the training set bags of centroids (for speed)
+train_centroids = np.zeros( (train["review"].size, num_clusters), \
+    dtype="float32" )
+
+# Transform the training set reviews into bags of centroids
+counter = 0
+for review in clean_train_reviews:
+    train_centroids[counter] = create_bag_of_centroids( review, \
+        word_centroid_map )
+    counter += 1
+
+# Repeat for test reviews 
+test_centroids = np.zeros(( test["review"].size, num_clusters), \
+    dtype="float32" )
+
+counter = 0
+for review in clean_test_reviews:
+    test_centroids[counter] = create_bag_of_centroids( review, \
+        word_centroid_map )
+    counter += 1
